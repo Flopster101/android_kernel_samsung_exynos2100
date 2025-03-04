Flopster101/android_kernel_samsung_universal8825

build: add wrapper script for ckbuild.sh
This automatically sets $WP to the previous level dir and calls ckbuild.sh, passing arguments to it.

Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

do_build.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+# Wrap to ckbuild.sh
+export WP=${WP:-$(realpath $PWD/../)}
+
+bash kernel_build/ckbuild.sh "$@"

Flopster101/android_kernel_samsung_exynos2100

do_build.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+# Wrap to ckbuild.sh
+export WP=${WP:-$(realpath $PWD/../)}
+
+bash build/ckbuild.sh "$@"